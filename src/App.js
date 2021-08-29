36f859f6e6bfb11de88c80e25b47e254ef3c4567
mini2809 <minijain928@gmail.com>
2021-08-29 23:27:09 +0530

    Started designing using material ui

--- src/App.js
+++ src/App.js
@@ -3,24 +3,54 @@ import { connect } from "react-redux";
 import { SaveLogin } from "./redux/login/login.actions";
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
-
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import MenuIcon from '@material-ui/icons/Menu';
+import { Typography } from "@material-ui/core";
+import  Container  from "@material-ui/core/Container";
+import  TextField  from "@material-ui/core/TextField";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import SearchIcon from "@material-ui/icons/Search";
+import IconButton from "@material-ui/core/IconButton";
 //login 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles({
   root: {
-    '& > *': {
-      margin: theme.spacing(1),
-    },
+   marginTop:120,
   },
-}));
+});
+
 const App=(props)=>{
-  const classes = useStyles();
+ const classes= useStyles();
     return(
-        <div>
-          <Button variant="contained" color="primary" onClick={() => props.saveLogin1()}>
+        
+          <Container >
+          <AppBar >
+            <Toolbar>
+              <MenuIcon/>
+              <Typography>Settings</Typography>
+            
+            <Button variant="contained" color="primary" onClick={() => props.saveLogin1()}>
             Login
           </Button>
-           
-        </div>
+          </Toolbar>
+          </AppBar>
+         <TextField className={classes.root}  
+         style = {{width: "100%"}} 
+         variant="outlined" 
+         label="Search" 
+         color="primary"
+         
+         InputProps={{
+          endAdornment: (
+            <InputAdornment>
+              <IconButton>
+                <SearchIcon />
+              </IconButton>
+            </InputAdornment>
+          ),
+        }}></TextField>
+          </Container>
+        
     )
 }
 
